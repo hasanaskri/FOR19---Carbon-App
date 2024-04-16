a44615e7ab0c0b63a49c1b3ef18949d972b89e12
Fredrik Waaler <fredrik.waaler@twoday.com>
2024-04-16 20:31:20 +0200

    Commit Chapter 2 & 3

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
 
 from flask import Flask
-application = Flask(_name_)
+application = Flask(__name__)
 
 @application.route('/')
 @application.route('/home')
 def home():
     return "<h1>Home page</h1>" \
-        "<p>In this project we develop 
-        and app...</p>"
+        "<p>In this project we develop and app...</p>"
 
 @application.route('/developers')
 def developers():
     return "<h1>Developers</h1>" \
-        "<p>In this webpage, we introduce 
-        the developers of the app...</p>"
+        "<p>In this webpage, we introduce the developers of the app...</p>"
 
 @application.route('/app_calculator')
 def app_calculator():
     return "<h1>App Calculator</h1>" \
-        "<p>In this webpage, we develop an 
-        app to work out transport carbon e
-        missions...</p>"
+        "<p>In this webpage, we develop an app to work out transport carbon emissions...</p>"
 
-if _name_ == '_main_':
+if __name__ == '_main_':
     application.run(debug=True)
