d4b2f2b11d06557273b747ebff94c18a7139376b
Fredrik Waaler <fredrik.waaler@twoday.com>
2024-04-16 20:20:06 +0200

    Initial Commit - Chapter 1

---
+++ app.py
@@ -0,0 +1,26 @@
+
+from flask import Flask
+application = Flask(_name_)
+
+@application.route('/')
+@application.route('/home')
+def home():
+    return "<h1>Home page</h1>" \
+        "<p>In this project we develop 
+        and app...</p>"
+
+@application.route('/developers')
+def developers():
+    return "<h1>Developers</h1>" \
+        "<p>In this webpage, we introduce 
+        the developers of the app...</p>"
+
+@application.route('/app_calculator')
+def app_calculator():
+    return "<h1>App Calculator</h1>" \
+        "<p>In this webpage, we develop an 
+        app to work out transport carbon e
+        missions...</p>"
+
+if _name_ == '_main_':
+    application.run(debug=True)
